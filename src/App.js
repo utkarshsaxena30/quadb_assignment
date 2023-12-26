972e320d09f5d5837aeab6de9d08b70888aa2cfa
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-12-26 10:37:08 +0530

    Change blank page route to merge into existing SPA

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import ShowList from './components/ShowList';
-import ShowDetails from './components/ShowDetails';
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ShowList from './components/ShowList'
+import ShowDetails from './components/ShowDetails'
 
 const App = () => {
-  const [shows, setShows] = useState([]);
+  const [shows, setShows] = useState([])
 
   useEffect(() => {
     // Fetch data from the API
     fetch('https://api.tvmaze.com/search/shows?q=all')
       .then((response) => response.json())
-      .then((data) => setShows(data));
-  }, []);
+      .then((data) => setShows(data))
+  }, [])
 
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/" exact>
+          <Route path='/' exact>
             <ShowList shows={shows} />
           </Route>
-          <Route path="/show/:id" component={ShowDetails} />
+          <Route path={`/show/:id`} exact>
+            <ShowDetails />
+          </Route>
         </Switch>
       </div>
     </Router>
-  );
-};
+  )
+}
 
 export default App
--- src/components/ShowList.js
+++ src/components/ShowList.js
@@ -1,40 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link, Route } from 'react-router-dom'
+import { useRouteMatch } from 'react-router-dom/cjs/react-router-dom'
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min'
 
 // Function to generate a random image URL
 const getRandomImage = () => {
-  const randomImageNumber = Math.floor(Math.random() * 1000) + 1;
-  return `https://picsum.photos/200/300?random=${randomImageNumber}`;
-};
+  const randomImageNumber = Math.floor(Math.random() * 1000) + 1
+  return `https://picsum.photos/200/300?random=${randomImageNumber}`
+}
 
 const ShowList = ({ shows }) => {
   return (
-    <div style={{backgroundColor:'black'}}>
-      <header style={{ backgroundColor: '#e50914', padding: '10px', textAlign: 'center' }}>
+    <div style={{ backgroundColor: 'black' }}>
+      <header
+        style={{
+          backgroundColor: '#e50914',
+          padding: '10px',
+          textAlign: 'center',
+        }}
+      >
         <h1 style={{ color: 'white' }}>Show List</h1>
       </header>
-      <ul style={{ listStyleType: 'none', padding: 0, display: 'flex', flexWrap: 'wrap', marginBottom:'0px' }}>
+      <ul
+        style={{
+          listStyleType: 'none',
+          padding: 0,
+          display: 'flex',
+          flexWrap: 'wrap',
+          marginBottom: '0px',
+        }}
+      >
         {shows.map((show) => (
-          <li key={show.show.id} style={{ margin: '10px', flex: '1 0 200px', textAlign: 'center' }}>
+          <li
+            key={show.show.id}
+            style={{ margin: '10px', flex: '1 0 200px', textAlign: 'center' }}
+          >
             <Link
-              to={`/show/${show.show.id}`}
+              to={`show/${show.show.id}`}
               style={{ textDecoration: 'none', color: '#333' }}
-              target="_blank" // Add this line to open link in a new window
-              rel="noopener noreferrer" // Recommended for security reasons
+              // target="_blank" // Add this line to open link in a new window
+              // rel="noopener noreferrer" // Recommended for security reasons
             >
               <img
-                src={show.show.image ? show.show.image.medium : getRandomImage()}
+                src={
+                  show.show.image ? show.show.image.medium : getRandomImage()
+                }
                 alt={show.show.name}
-                style={{ maxWidth: '100%', height: 'auto', marginBottom: '10px' }}
+                style={{
+                  maxWidth: '100%',
+                  height: 'auto',
+                  marginBottom: '10px',
+                }}
               />
-              <div style={{color:'white'}}>{show.show.name}</div>
-              <div style={{color:'white'}}>{show.show.premiered}</div>
+              <div style={{ color: 'white' }}>{show.show.name}</div>
+              <div style={{ color: 'white' }}>{show.show.premiered}</div>
             </Link>
           </li>
         ))}
       </ul>
     </div>
-  );
-};
+  )
+}
 
-export default ShowList;
+export default ShowList
